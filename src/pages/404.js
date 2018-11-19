60f03a54bc6835690404556b3c402c8c9037ec27
Eric Leung <eric@erictleung.com>
2018-11-19 14:14:16 -0800

    Add a 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+class NotFoundPage extends React.Component {
+  render() {
+    return (
+      <Layout location={this.props.location}>
+        <h1>404 Page No Found</h1>
+        <p>
+          Oh bother! Sorry, we've misplaced that URL or it's pointing to
+          something that doesn't exist. <Link to="/">Head back home</Link> to
+          try finding it again.
+        </p>
+      </Layout>
+    )
+  }
+}

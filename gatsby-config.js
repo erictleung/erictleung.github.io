5205c162c1099c96a6b6c8ed098cf68aae5ad72f
Eric Leung <eric@erictleung.com>
2018-11-19 14:02:49 -0800

    Access file system to build a page
    This commit queries all the files in the src/ directory and outputs
    various information about the file.
    
    This is to demonstrate how to query using GraphQL and then build a page
    from it.

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,13 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography.js`,

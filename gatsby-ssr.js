2d19b6b16d62d5ffc261f0c62154788ca71bf791
Eric Leung <eric@erictleung.com>
2018-11-26 01:58:58 -0800

    Remove unsupported configuration

--- gatsby-ssr.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-exports.onRenderBody = ({ setPreBodyComponents }) => {
-  setPreBodyComponents([
-    <noscript key="noscript">
-      Your browser does not support JavaScript!
-    </noscript>,
-  ])
-}

9f76b3fd19dc9d0e866049967de69d802255e02e
Eric Leung <eric@erictleung.com>
2018-12-01 22:55:09 -0800

    Fix relative paths for blog posts
    This commit adds the /blog to be added to the slug.

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,15 +2,27 @@ const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
+  const { createNodeField } = actions;
 
-  // Finds file node and create slug
+  // Finds file node, extend node, and creates slug
   if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
+
+    // Create the URL for the page
+    const slug = createFilePath({
+      // node = node you'd like to convert to a path
+      node,
+      // getNode = method to get node
+      getNode,
+      // basePath = base path for your files
+      basePath: `pages`,
+      // Trailing "/" or not
+      trailingSlash: false
+    })
+
     createNodeField({
       node,
       name: `slug`,
-      value: slug
+      value: `/blog${slug}`
     })
   }
 }
@@ -36,7 +48,9 @@ exports.createPages = ({ graphql, actions }) => {
           path: node.fields.slug,
           component: path.resolve(`./src/templates/blog-post.js`),
           context: {
-            slug: node.fields.slug,
+            // Data passed to context is available in page queries as GraphQL
+            // variables
+            slug: node.fields.slug
           },
         })
       })
